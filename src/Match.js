0d958e9a13d7d68b648768b5d97b6e7b8a5cd854
rileymci <riley.mcilwain@connect.qut.edu.au>
2024-03-04 17:35:58 +1000

    Index and login page setup

---
+++ src/Match.js
@@ -0,0 +1,9 @@
+export default function Match() {
+    return(
+        <div className="post">
+          <img src="" alt=""></img>
+          <h2>testing testing tesing</h2>
+          <p>bla bla bla bla bla bla bla</p>
+        </div>
+    );
+}
