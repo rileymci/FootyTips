b53ce1dc10a1dc0285e79d52cf677465f3f7c888
rileymci <riley.mcilwain@connect.qut.edu.au>
2024-03-08 21:45:16 +1000

    Login and register authentication

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./pages/UserContext";
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<Layout />}> 
-        <Route index element={<IndexPage />} />
-        <Route path={'/login'} element={<LoginPage />} />
-        <Route path={'/register'} element={<RegisterPage />} />
+    <UserContextProvider>
+        <Routes>
+          <Route path='/' element={<Layout />}> 
+          <Route index element={<IndexPage />} />
+          <Route path={'/login'} element={<LoginPage />} />
+          <Route path={'/register'} element={<RegisterPage />} />
         </Route>
       </Routes>
+    </UserContextProvider>
+
     
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,47 @@
 import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./pages/UserContext";
 
 export default function Header() {
+    const {setUserInfo, userInfo} = useContext(UserContext);
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include',
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUserInfo(userInfo);
+            });
+        });
+    }, []);
+
+    function logout(){
+        fetch('http://localhost:4000/logout', {
+            credentials: 'include',
+            method: 'POST',
+        });
+        setUserInfo(null);
+    }
+
+    //? is used because sometimes userinfo/ username can be null.
+    const username = userInfo?.username;
+
     return(
         <header>
         <Link to="/" className="logo">My Tips</Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+            {username && (
+                <>
+                <Link to="/create">Create new post</Link>
+                <a onClick={logout}>Logout</a>
+                </>
+            )}
+            {!username && (
+                <>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+                </>
+            )}
+
         </nav>
       </header>
     );
